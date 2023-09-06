83153be0c846c10ccb795dcb247750a1ed3aca0e
Ruovagems <kelechukwufavour.faye@gmail.com>
2023-09-06 23:12:24 +0000

    files

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+# Ruovagems <KELECHUKWU FAVOUR>
+# 0-rectangle.py
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
 	"""Represent a rectangle."""
     pass
-
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""
+
+This module includes a class that defines a Rectangle.
+
+"""
 class Rectangle:
+	"""Class that defines a Rectsngle."""
     def __init__(self, width=0, height=0):
+	""" Method that initializes the instance."""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+	"""Method that returns the value of the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+	"""Method that defines the width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -18,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+	"""Method that returns the value of a height.""""
         return self.__height
 
     @height.setter
     def height(self, value):
+	"""Method that defines the value of a height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
