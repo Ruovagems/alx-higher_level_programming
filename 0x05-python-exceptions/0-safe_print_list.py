2ca339089757d7cd27b92d6f2ed9a3f22d66a6df
Ruovagems <kelechukwufavour.faye@gmail.com>
2023-09-06 21:50:03 +0000

    corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
-        if i < len(my_list):
+        try:
             print(f"{my_list[i]}", end="")
             total += 1
-        else:
+        except IndexError:
             break
     print()
-    return total
+    return(total)
