47373a96f38be6cbfd10bd6b1f69f5a5dcb978c6
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2023-11-14 07:12:51 -0800

    corrections

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-const dict = require('./101-data');
-
-const userOccurrenceDict = {};
-
-for (const userId in dict) {
-  const occurrence = dict[userId];
-  if (!(occurrence in userOccurrenceDict)) {
-    userOccurrenceDict[occurrence] = [];
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  userOccurrenceDict[occurrence].push(userId);
 }
-
-console.log(userOccurrenceDict);
+console.log(newDict);
