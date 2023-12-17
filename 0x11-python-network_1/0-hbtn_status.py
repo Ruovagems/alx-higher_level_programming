8dbe24eade108c31c1db840ec1a5554d76226e15
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2023-12-17 15:28:17 -0800

    tsks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         content = response.read().decode('utf-8')
 
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
