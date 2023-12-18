184e2aebc035cf8c9ed1f9096f506d57eb4facaa
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2023-12-18 20:01:49 +0000

    push

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,22 +7,28 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     us = sys.argv[1]
     ps = sys.argv[2]
     dbs = sys.argv[3]
     name = sys.argv[4]
 
-    db = MySQLdb.connect(
-         host="localhost", port=3306 user=us, passwd=ps, db=dbs)
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY \
-    '{}' ORDER BY id ASC".format(name))
+    try:
+        # Explicitly set host to "localhost" and use default socket
+        db = MySQLdb.connect(
+            host="localhost", port=3306, user=us, passwd=ps, db=dbs, unix_socket="/var/run/mysqld/mysqld.sock"
+        )
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY \
+            '{}' ORDER BY id ASC".format(name))
 
-    rows = cursor.fetchall()
+        rows = cursor.fetchall()
 
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+        for row in rows:
+            print(row)
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
