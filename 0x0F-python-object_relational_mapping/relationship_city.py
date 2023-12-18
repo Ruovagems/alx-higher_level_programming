3a655a4ad75f41e8ba233ff715710ff417533bc7
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2023-12-18 21:55:45 +0000

    task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-# Defines a City model.
-# Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+"""Module that contains the class definition of City and instance Base"""
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
-
-    Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    """City class that inherits from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+# Add necessary import
+from sqlalchemy.orm import relationship
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
-from sqlalchemy import Column, Integer, String
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
 
+Base = declarative_base()
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
     """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    Class with the name and id attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", back_populates="state")
 
-    cities = relationship("City", backref="state", cascade="all, delete")
+if __name__ == "__main__":
+    # Modify your connection details
+    db_user = "your_mysql_username"
+    db_password = "your_mysql_password"
+    db_host = "localhost"
+    db_name = "hbtn_0e_100_usa"
+    
+    engine = create_engine(f"mysql://{db_user}:{db_password}@{db_host}/{db_name}")
+    Base.metadata.create_all(engine)
